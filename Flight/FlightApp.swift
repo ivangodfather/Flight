820fad534859dca55eb42243da26e41ed0041005
Ivan Ruiz <ruiz@fueled.com>
2023-10-16 22:12:40 +0200

    Initial Commit

---
+++ Flight/FlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightApp.swift
+//  Flight
+//
+//  Created by Ivan on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
